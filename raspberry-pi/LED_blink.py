b367aa0a41027e0624d0f604b54127c6d1502470
jmuss07 <jmuss07@charlottesvilleschools.org>
2022-09-02 13:28:10 -0400

    Updated code for onboard LED to blink

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -3,3 +3,10 @@ import time
 import digitalio
 
 led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    time.sleep(.1)
+    led.value = False
+    time.sleep(.1)
