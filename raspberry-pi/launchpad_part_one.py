1da1693d1a64b4009f472cb86b9f845f17328cc5
jmuss07 <jmuss07@charlottesvilleschools.org>
2022-09-02 13:59:07 -0400

    added comments to code

--- raspberry-pi/launchpad_part_one.py
+++ raspberry-pi/launchpad_part_one.py
@@ -1,9 +1,9 @@
 import board
 import time
 
-for i in range(10, -1, -1):
-    if i == 0:
-        print("Liftoff!")
-    else:
-        print(i)
-        time.sleep(1)
+for i in range(10, -1, -1): #creates a range of numbers from 10 to -1, but does not inbclude -1 in the list; also indicates that we're counting down by 1
+    if i == 0: #only runs if the value of the counter is 0
+        print("Liftoff!") #prints "Liftoff!"
+    else: #runs if the counter is at any other value in the range
+        print(i) #prints the current value of the counter
+        time.sleep(1) #waites for 1 second before repeating
