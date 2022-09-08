216bf34e0610177e52c5fe0ebf344306243ad6c8
jmuss07 <jmuss07@charlottesvilleschools.org>
2022-09-08 14:04:56 -0400

    created code for launchpad pt 2

---
+++ raspberry-pi/launchpad_part_two.py
@@ -0,0 +1,19 @@
+import board
+import time
+import digitalio
+
+red = digitalio.DigitalInOut(board.GP14) #sets pin to connect red LED to board
+red.direction = digitalio.Direction.OUTPUT #sets pin type
+green = digitalio.DigitalInOut(board.GP15) #sets pin to connect green LED to board
+green.direction = digitalio.Direction.OUTPUT #sets pin type
+
+for i in range(10, 0, -1): #creates a range of numbers from 10 to 0, but does not include 0 in the list; also indicates that we're counting down by 1
+  red.value = True #turns red LED on
+  green.value = False #turns green LED off
+  print(i) #prints the current value of the counter
+  red.value = False #turns red LED off
+  time.sleep(1) #waites for 1 second before repeating
+print("Liftoff!") #prints the phrase "Liftoff!" one second after we exit the range of numbers in the for-loop
+while True:
+  green.value = True #turns green LED on
+        
