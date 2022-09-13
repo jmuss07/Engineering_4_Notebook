66d994b586fb04542783e10a727707c1c763f1dc
jmuss07 <jmuss07@charlottesvilleschools.org>
2022-09-13 14:20:53 -0400

    finalized servo code

--- raspberry-pi/launchpad_part_four.py
+++ raspberry-pi/launchpad_part_four.py
@@ -13,17 +13,18 @@ button.pull = digitalio.Pull.UP
 pwm_servo = pwmio.PWMOut(board.GP18, duty_cycle = 2 ** 15, frequency = 50) #defines values for general pwmio servo
 launch_servo = servo.Servo(pwm_servo, min_pulse = 500, max_pulse = 2500) #sets up parameters for specific servo used for launch
 
+print("code running!")
+
 while True:
     if button.value == False: #if button is pressed...
         for i in range(10, 0, -1): #creates a range of numbers from 10 to 0, but does not include 0 in the list; also indicates that we're counting down by 1
-            launch_servo.angle = 0
+            launch_servo.angle = 0 #sets servo angle to 0 degrees
             red.value = True #turns red LED on
             green.value = False #turns green LED off
             print(i) #prints the current value of the counter
             red.value = False #turns red LED off
             time.sleep(1) #waites for 1 second before repeating
         print("Liftoff!") #prints the phrase "Liftoff!" one second after we exit the range of numbers in the for-loop
-        while True:
-            green.value = True #turns green LED on
-            launch_servo.angle = 180
+        green.value = True #turns green LED on
+        launch_servo.angle = 180 #sets servo angle to 180 degrees
             
