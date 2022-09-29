b571728898026c899bff976181b9bdb96e1929f4
jmuss07 <jmuss07@charlottesvilleschools.org>
2022-09-29 14:11:33 -0400

    finished code for LAP1

--- raspberry-pi/Landing Area/landing_area_part_one.py
+++ raspberry-pi/Landing Area/landing_area_part_one.py
@@ -1,6 +1,21 @@
 import board
 import time
 
+def triangle_area(list_1, list_2, list_3):
+    try:
+        Ax = float(list_1[0])
+        Ay = float(list_1[1])
+        Bx= float(list_2[0])
+        By = float(list_2[1])
+        Cx = float(list_3[0])
+        Cy = float(list_3[1])
+        area = abs(Ax*(By-Cy) + Bx*(Cy - Ay) + Cx*(Ay - By))/2
+        print(f"\nThe area of the triangle with vertices ({Ax},{Ay}), ({Bx},{By}), ({Cx},{Cy}) is {area} square km!")
+        return area
+    except:
+        print("\nSorry, these points do not form a valid triangle. Please try again, and make sure you are using the x,y sintax!")
+                
+
 cont = False
 while not cont:
     start = input("\nType 'start' to begin, or type -q to quit: ").lower()
@@ -9,26 +24,10 @@ while not cont:
     else:
         coord_1 = input("\nInput first coordinate in format x,y: ")
         list_1 = coord_1.split(",")
-        float(list_1[0])
-        float(list_1[1])
+
         coord_2 = input("\nInput second coordinate in format x,y: ")
         list_2 = coord_2.split(",")
-        float(list_2[0])
-        float(list_2[1])
+
         coord_3 = input("\nInput third coordinate in format x,y: ")
         list_3 = coord_3.split(",")
-   
-   
-        
-'''
-        = input(f"\nIs '{phrase}' correct? (Y/N)\t")
-        if confirm.lower() == "y":
-            print()
-            for i in phrase:
-                print(i)
-                if i not in asl_dict:
-                    print(f"Sorry, I don't know how to sign {i}")
-                else:
-                    asl_dict[i]()
-                    time.sleep(1)
-'''
+        triangle_area(list_1, list_2, list_3)
